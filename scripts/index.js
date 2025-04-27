7e579b2e47fcf585c296db90e983bc7e175cbb71
Magali <130312385+galima10@users.noreply.github.com>
2025-04-27 22:41:51 +0200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector("h1").style.fontSize = "100px";
+document.querySelector("#footer").style.width = "100px";
+document.querySelector("#footer").style.height = "100px";
+document.querySelector("#footer").style.background = `url(media/images/tableau-1.webp)`;
+document.querySelector(".img-footer").src = "media/images/logos/favicon.webp";
